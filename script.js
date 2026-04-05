5b32a63aeb5fb050a1d8753e098d97819c0214b5
Mehrouf22 <mehrouf22@gmail.com>
2026-04-04 23:38:13 -0700

    qn 1 solved

--- script.js
+++ script.js
@@ -9,7 +9,33 @@
 // Day-4
 // Practice qn and then we moving to loops and all that 
 
-
-
+// function checkNumber(num) {
+// if(num<=0) return "Invalid number"
+// let result="";
+// if(num%2===0){
+//     result += "Even "
+// }else{
+//     result += "Odd "
+// }
+// if(num>100){
+//     result += "Huge "
+// }else if(num>50){
+//     result += "Large "
+// }else{
+//     result += "Small "
+// }
+// if(num%3===0){
+//     result += "Divisible my 3, ";
+// }else{
+//     result += "Not divisible by 3 ";
+// }
+// if(num%5===0){
+//     result += "Divisible by 5";
+// }else{
+//     result += "Not divisible by 5,";
+// }
+// return result;
+// }
+// console.log(checkNumber(3));
 
 
