c992fa1f0bb6e0981ec2dd1c9204d6b6a8ccc986
Mehrouf22 <mehrouf22@gmail.com>
2026-04-17 13:50:05 -0700

    bli

--- script.js
+++ script.js
@@ -256,9 +256,278 @@
 //     console.log(i);
 //    } 
 // }
-
+ 
 // for(let i=1;i<51;i++){
 //     if(i%3===0){
 //         console.log(i);
 //     }
 // }
+
+// let num=prompt("Enter the number");
+// for(let i=1;i<=num;i++){
+//     if(i%2===0){
+//         console.log(i +" is an even number");
+//     }
+//     else{
+//         console.log(i+ " is an odd number");
+//     }
+// }
+// console.log(num);
+
+//  for(let i=1;i<101;i++){
+//     console.log(i);
+//     if(i%7===0){
+//         break;
+//     }
+//  }
+
+// for(let i=1;i<21;i++){
+//     if(i%3===0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let count=0;
+// for( let i=1;i<101;i++){
+//     if(i%2===1){
+//         count++;
+//         console.log(i)
+//     }
+//     if(count===5)break;
+// }
+
+// for(let i=1;i<21;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// let i=1;
+// while(i<6){
+//     console.log(i);
+//     i++
+// }
+
+// for(let i=10;i>1;i--){
+//     console.log(i)
+// }
+
+// for(let i=10;i>0;i--){
+//     console.log(i);
+// }   
+
+// let i=2;
+// while(i<11){
+// if(i%2===0){
+//     console.log(i)
+// }
+//     i++
+// }
+
+// for(let i=1;i<16;i++){
+//     if(i%2===1){
+//         console.log(i)
+//     }
+// }
+// let multi=5;
+// let i=1;
+// while(i<11){
+// console.log(multi +"x"+i+"="+multi*i)
+//     i++
+// }
+// let num=0
+// for(let i=1;i<6;i++){
+// num=num+i;
+
+// }
+// console.log(num);
+
+// let sum=0;
+// for(let i=1;i<6;i++){
+//     sum=sum+i;
+
+// }
+// console.log(sum);
+
+// let sum=0;
+// let i=2;
+// while(i<11){
+//     if(i%2===0){
+// sum=sum+i;
+//     }
+    
+//     i++;
+// }
+// console.log(sum);
+
+// let count=0
+// for(let i=1;i<21;i++){
+//     if(i%2===0){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// let sum=0;
+// let i=1;
+// while(i<16){
+//     if(i%2!==0){
+// sum=sum+i;
+//     }
+//     i++;
+// }
+// console.log(sum);
+
+// Functions
+
+// function greet(){
+//     console.log("hello mehrouf")
+// }
+// greet();
+
+// let a=()=>{
+//     console.log("Sub bitch")
+// }
+// a();
+
+// function abcd(){
+//     for(let i=1;i<11;i++){
+//         if(i%2===0){
+//             console.log(i);
+//         }
+//     }
+// }
+// abcd();
+
+// function ab(v1,v2){
+//     console.log(v1*v2)
+// }
+// ab(10,20)
+
+// function welcomeuser(name=guest){
+// console.log("welcome "+ name);
+// }
+// welcomeuser();
+// welcomeuser("Mehrouf");
+
+
+// let a=(name="Guest")=>{
+// console.log("hi "+name);
+// }
+// a();
+// a("Mehrouf");
+
+// let showAge=(name="Age")=>{
+//     console.log(name);
+// }
+// showAge();
+// showAge(24);
+
+// function pricetag(tag=100){
+// console.log("Price: "+tag);
+// }
+// pricetag();
+// pricetag(250);
+
+// function playerhealth(health=100){
+//     console.log("Health = "+health);
+// }
+// playerhealth();
+// playerhealth(75);
+
+// let multiply=(num1=2,num2=3)=>{
+// console.log(num1*num2);
+// }
+// multiply();
+// multiply(4,5);
+
+// function abcd(...a){
+// console.log(a);
+// }
+// abcd(1,3,4,5,6,7)
+
+// let sum = (a,b)=>{
+//    return a + b;
+// }
+
+// // console.log(sum(2,3));
+// function greet(Person="Guest"){
+// return "welcome "+ Person;
+// }
+// console.log(greet("Mehrouf"));
+
+// let a=(square,hi)=>{
+//     console.log(square*hi);
+// }
+// a(4,2);
+
+// let num=prompt("whats the number");
+// let a=()=>{
+//     if(num%2===0){
+//         console.log("Even number")
+//     }else{
+//         console.log("Odd number")
+//     }if(num===""){
+//         console.log("Enter a number")
+//     }
+// }
+// a();
+
+// let sum=0;
+// function add(num1,num2){
+// sum=num1+num2;
+// return sum;
+// }
+// console.log(add(10,20));
+
+// function multiply(val1,val2){
+// return val1*val2;
+// }
+// console.log(multiply(4,5));
+// console.log(multiply(3,2));
+
+// function CheckEven(num){
+// if(num%2===0){
+//     return "Even"
+// }
+// else{
+//     return "Odd"
+// }
+
+    
+// }
+// console.log(CheckEven(4));
+// console.log(CheckEven(7));
+
+// function isPostive(num){
+//     if(num>0){
+//         return "Postive"
+//     }else if(num<0){
+//         return "negative"
+//     }else{
+//         return "zero"
+//     }
+// }
+// console.log(isPostive(4));
+// console.log(isPostive(-4));
+// console.log(isPostive(0));
+
+// for(let i=1;i<11;i++){
+//     console.log(i)
+// }
+
+// let square=(n)=>{
+//    return n*n;
+// }
+// console.log(square(4));
+
+// function greet(name="Guest"){
+// if(name===""){
+//     return "welcome guest"
+// }else{
+//     return "welcome "+name;
+// }
+// }
+// console.log(greet( ));
+
